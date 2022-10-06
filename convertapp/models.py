00f0fad4f1567bf91add67d8ba880ec78c3698e1
Hamdy Adam <hamdyadam97@gmail.com>
2022-10-06 05:11:37 +0200

    robottk

--- convertapp/models.py
+++ convertapp/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
 # Create your models here.
-from gdstorage.storage import GoogleDriveStorage
-
-# Define Google Drive Storage
-gd_storage = GoogleDriveStorage()
-
 
 class ConvertAudio(models.Model):
     link_video = models.CharField(max_length=250)
     link = models.CharField(max_length=220,null=True)
-    map_data = models.FileField(upload_to='maps',null=True)
+
     def __str__(self):
         return format(self.link)
